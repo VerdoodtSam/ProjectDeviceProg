b10af23eef91214cdd50104feefced7faf09f47b
VerdoodtSam <sam.verdoodt@student.howest.be>
2021-12-10 11:56:51 +0100

    Prep layout files

---
+++ ProjectSamVerdoodt/ProjectSamVerdoodt/Repositories/APIRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectSamVerdoodt.Repositories
+{
+    public static class APIRepo
+    {
+        private const string _APIKEY = "...";
+        private const string _USERTOKEN = "...";
+    }
+}
